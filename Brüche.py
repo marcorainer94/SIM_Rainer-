3b208afa7ec4f424480b69be95ed1812d3afb03c
Marco <rainer.marco@gmx.at>
2020-11-09 17:21:06 +0100

    Convert to library

--- Brüche.py
+++ Brüche.py
@@ -11,36 +11,4 @@ def add_frac(z1,n1,z2,n2):
     c=[a[0]+b[0],a[1]]
     return c
 
-while True:
-    try:
-        z1=int(input("Geben Sie den ersten Zähler als ganze Zahl ein: "))
-        break 
-    except ValueError:
-        print("Das ist keine ganze Zahl! Bitte nochmals versuchen!")
-        
-while True:
-    try:
-        n1=int(input("Geben Sie den ersten Nenner als ganze Zahl ein: "))
-        break 
-    except ValueError:
-        print("Das ist keine ganze Zahl! Bitte nochmals versuchen!")
 
-while True:
-    try:
-        z2=int(input("Geben Sie den zweiten Zähler als ganze Zahl ein: "))
-        break 
-    except ValueError:
-        print("Das ist keine ganze Zahl! Bitte nochmals versuchen!")
-        
-while True:
-    try:
-        n2=int(input("Geben Sie den zweiten Nenner als ganze Zahl ein: "))
-        break 
-    except ValueError:
-        print("Das ist keine ganze Zahl! Bitte nochmals versuchen!")   
-        
-
-        
-c=add_frac(z1,n1,z2,n2)
-print("Zähler", c[0])
-print("Nenner", c[1])
