fca096d9913b5dfde3fd84125d4a6f373ca165b5
Marco <rainer.marco@gmx.at>
2020-11-09 17:07:16 +0100

    Add test

---
+++ test_Brüche.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  9 16:49:29 2020
+
+@author: raine
+"""
+
+from Brüche import add_frac 
+
+def test_add_frac(): 
+    assert add_frac(2,3,4,5) == [22, 15]
+
+     
+  
